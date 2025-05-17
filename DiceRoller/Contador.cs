a15d80aa3254b6bc06715db30f650942198eb302
cabrera0laura <lucas56hoseok@gmail.com>
2025-05-16 22:19:36 -0300

    Logica do DiceRoller - sequencia, e pontos

---
+++ DiceRoller/Contador.cs
@@ -0,0 +1,23 @@
+﻿
+namespace DiceRoller
+{
+    public class Contador
+    {
+        public int Sequencia;
+        public int Pontos;
+        public int Limite;
+        public String Venceu;
+
+        //              classe --> nomeCLasse    
+        public void Jogar(Dice dadoJogador, int numeroSelecionado )
+        {
+            // manipular a informação do atributo
+            // comparar se o resultado (facePraCima) e o mesmo que foi selecionado
+            // Se for igual o  jogador venceu
+            // soma +1 ponto
+            // Se ele tinha vencido o duelo anterior, soma 1 na sequencia
+            // Se não, 0 a sequencia.
+         
+        }
+    }
+}
---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,33 @@
+﻿
+namespace DiceRoller
+{
+    public class Dice
+    {
+        //       private String faceParaBaixo;  
+        //       public string FaceParaBaixo { get => faceParaBaixo; set => faceParaBaixo = value; } //*
+
+        //Atributos -Properties
+        public int FaceParaBaixo;
+        public int FaceParaCima; // posso escrever diretamente o atributo com letra maiuscula o VS2022 entende. 
+        public int QuantidadeDeFaces;
+        public bool 
+
+        public Dice(int faces)
+        {
+            QuantidadeDeFaces = faces;
+        }
+
+        //Métodos
+        public void Sortear()
+        {
+            //Gero um numero aleatorio
+            Random random = new Random();
+            //jogo esse numero na face para cima
+            FaceParaCima = random.Next(1, 7);
+
+            //Soma magica = 1 + maior quantidadeDeFaces (7).
+            FaceParaBaixo = 1+ QuantidadeDeFaces - FaceParaCima ;
+            //Calcular a face para baixo
+        }
+    }
+}
